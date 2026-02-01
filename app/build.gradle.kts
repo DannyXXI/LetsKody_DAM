68d3c1d5fdfdbe0c89cb569155467aae1cab1c77
Daniel Requejo <daniel.reqexp@gmail.com>
2026-02-01 15:03:52 +0100

    Se agregan las librerias al proyecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp") version "2.3.4"
+    alias(libs.plugins.google.gms.google.services)  // plugin ksp (agregar primero y sincronizar)
 }
 
 android {
@@ -38,6 +40,33 @@ android {
 }
 
 dependencies {
+    // librerias para Material3
+    implementation("androidx.compose.material3:material3:1.4.0") // dependencia de actualización
+
+    // librerias para iconos
+    implementation("androidx.compose.material:material-icons-extended:1.5.0-alpha11") // dependencia para iconos de formularios
+    implementation("com.composables:icons-lucide:1.1.0")                              // dependencia para la libreria de iconos Lucide
+
+    // librerias para usar la base de datos local (Room - SQLite)
+    implementation("androidx.room:room-runtime:2.8.4") // dependencia principal para poder usar Room
+    ksp("androidx.room:room-compiler:2.8.4")           // dependencia que permite usar el procesador de anotaciones de Room (KSP)
+    implementation("androidx.room:room-ktx:2.8.4")     // dependencia que agrega extensiones de Kotlin para Room
+    implementation("io.coil-kt:coil-compose:2.7.0")    // dependencia para usar imagenes desde Internet (AsyncImage)
+
+    // librerias para el uso de notificaciones
+    implementation(platform("androidx.compose:compose-bom:2025.12.00")) // dependecia para usar notificaciones en el Snackbar
+    implementation("com.google.accompanist:accompanist-permissions:0.31.1-alpha") // dependencia para usar notficaciones con icono
+
+    // librerias para usar Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))  // dependencia para usar el boom de Firebase
+    implementation("com.google.firebase:firebase-auth-ktx")                        // dependencia para usar la autenticacion de Firebase
+    implementation(libs.firebase.firestore)                                        // dependencia para usar la base de datos de Firebase
+
+    // librerias para la navegacion entre pantallas
+    implementation(libs.androidx.navigation.compose)  // dependencia para utilizar la navegacion entre pantallas
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.5.0-alpha11") // dependencia para la navegacion adaptativa segun el tamaño de pantalla
+    implementation("androidx.compose.material3:material3-window-size-class:1.4.0")                 // dependencia para usar herramientas para clasificar el tamaño de la pantalla
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
